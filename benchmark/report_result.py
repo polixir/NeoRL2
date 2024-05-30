75a24578a4a2985f14d3d61ce4bc95a4bdfb41a1
yi <songyigao@gmail.com>
2024-05-30 17:02:45 +0800

    .

--- benchmark/report_result.py
+++ benchmark/report_result.py
@@ -2,6 +2,9 @@ import os
 import glob
 import pandas as pd
 
+import neorl2
+import gymnasium as gym
+
 
 ResultDir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'results'))
 logs_files = glob.glob(os.path.join(ResultDir, '**/*.csv'), recursive=True)
@@ -30,4 +33,22 @@ for file_path in logs_files:
     df.loc[df.index== domain, algo] = str(log.loc[log['Reward_Mean_Env','mean'].idxmax()]['Reward_Mean_Env',"mean"].round(2))+"±"+str(log.loc[log['Reward_Mean_Env','mean'].idxmax()]['Reward_Mean_Env',"std"].round(2))
     
 # df.to_csv("./result.csv")
-print(df)
+print("Reward result:\n",df)
+
+
+for file_path in logs_files:
+    if "bc_model" in file_path:
+        continue
+    task = os.path.split(os.path.split(file_path)[0])[1]
+    domain, algo = task.split("-")
+    
+    log = pd.read_csv(file_path)
+    log = log[["Reward_Mean_Env","experiment_tag"]]
+    log['experiment_tag'] = process_column(log['experiment_tag'])
+    env = gym.make(domain)
+    log["Reward_Mean_Env"] = log["Reward_Mean_Env"].apply(env.get_normalized_score)
+    log = log.groupby(['experiment_tag']).agg({'Reward_Mean_Env': ['mean', 'std']})
+    df.loc[df.index== domain, algo] = str(log.loc[log['Reward_Mean_Env','mean'].idxmax()]['Reward_Mean_Env',"mean"].round(2))+"±"+str(log.loc[log['Reward_Mean_Env','mean'].idxmax()]['Reward_Mean_Env',"std"].round(2))
+    
+print("Score result:\n",df)
+    
